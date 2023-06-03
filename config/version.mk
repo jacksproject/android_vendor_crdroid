3e905eb1201a111e335575f6e92a19751dbedc89
jacksproject <contact@jackbd.me>
2023-06-03 12:27:12 +0000

    vendor: Add maintainer string
    Signed-off-by: jacksproject <contact@jackbd.me>

--- config/version.mk
+++ config/version.mk
@@ -4,8 +4,15 @@ PRODUCT_VERSION_MINOR = 0
 # Increase CR Version with each major release.
 CR_VERSION := 9.5
 
+# Default maintainer will show as UNKNOWN
+DEVICE_MAINTAINER ?= UNKNOWN
+
 # Internal version
 LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
 
 # Display version
 LINEAGE_DISPLAY_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+
+# Maintainer stuff
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+  ro.device.maintainer=$(DEVICE_MAINTAINER)
